03ae86d3688c112a91f92e15d487104e2a20024a
Rajat Nai <rajatnai49@gmail.com>
2023-08-05 11:22:06 +0530

    Complete Assignment-1 & add Readme file

---
+++ Assignment-01/randomized_quick_sort.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+
+int partition(vector<int> &arr, int left, int right)
+{
+    // Finding random element in range [left, right]
+    int random_number = left + (rand() % (right - left + 1));
+
+    // Swap random element with the last element so we can apply our simple quicksort
+    swap(arr[random_number], arr[right]);
+
+    // Normal quicksort
+    int i = left - 1, x = arr[right];
+    for (int j = left; j <= right - 1; j++)
+    {
+        if (arr[j] <= x)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[right]);
+    return i + 1;
+}
+
+void randomized_quick_sort(vector<int> &arr, int left, int right)
+{
+    if (left < right)
+    {
+        // Finding pivot element
+        int pivotIndex = partition(arr, left, right);
+
+        // D&C
+        randomized_quick_sort(arr, left, pivotIndex - 1);
+        randomized_quick_sort(arr, pivotIndex + 1, right);
+    }
+}
+
+int main()
+{
+    // Generate random number every time
+    srand((unsigned)time(NULL));
+
+    // Given array
+    vector<int> arr = {1, 2, 4, 1, 5, 4, 7, 5, 9, 4};
+    int n = arr.size();
+    cout << "Original Array: ";
+    for (auto x : arr)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    // Sorting the array using ranomized quicksort algorithm
+    randomized_quick_sort(arr, 0, n - 1);
+
+    cout << "Sorted Array: ";
+    for (auto x : arr)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Advanced Algorithms Lab Assignment
+
+| Assignment Number | Problems                                       | Link                     |
+| ----------------- | ---------------------------------------------- | ------------------------ |
+| Assignment - 01   | 1. Randomized Quicksort                        | [Link](./Assignment-01/) |
+| Assignment - 02   | 1. Primality Testing (Fermit's Algorithm)      | [Link](./Assignment-02/) |
+| Assignment - 03   | 1. Finding Max Flow (Ford-Fulkerson Algorithm) | [Link](./Assignment-03/) |
+| Assignment - 04   | 1. Finding Min Cut (Karger's Algorithm)        | [Link](./Assignment-04/) |

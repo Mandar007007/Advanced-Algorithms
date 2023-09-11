ec2bde5ce2c8024d572e29f92e6497ff8fd4163e
Rajat Nai <96529504+rajatnai49@users.noreply.github.com>
2023-09-11 17:23:42 +0530

    Create graham_scan.cpp

---
+++ Assignment-09/graham_scan.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Point {
+public:
+    int x;
+    int y;
+    Point(int x, int y): x(x), y(y) {}
+
+};
+
+int main() {
+    vector<Point> points;
+    points.push_back(Point(0,3));
+    points.push_back(Point(1,1));
+    points.push_back(Point(2,2));
+    points.push_back(Point(4,4));
+    points.push_back(Point(0,0));
+    points.push_back(Point(1,2));
+    points.push_back(Point(3,1));
+    points.push_back(Point(3,3));
+    
+    
+
+    return 0;
+}
